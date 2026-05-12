962b3de9e803f06cf51e6c8b2b0d94971b5666a6
Kiba1585 <122737594+Kiba1585@users.noreply.github.com>
2026-05-12 07:04:02 -0400

    Crear lib/main.dart

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'screens/activation_screen.dart';
+
+void main() {
+  runApp(const MipymeWindowsApp());
+}
+
+class MipymeWindowsApp extends StatelessWidget {
+  const MipymeWindowsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MIPYME Windows',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: const ActivationScreen(),
+    );
+  }
+}
