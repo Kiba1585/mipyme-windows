0cc56d244a639a46fea35b709b68a37c2c55c158
Kiba1585 <122737594+Kiba1585@users.noreply.github.com>
2026-05-13 02:07:47 +0200

    Refactor Asset class constructor and methods

--- lib/models/asset.dart
+++ lib/models/asset.dart
@@ -5,10 +5,32 @@ class Asset {
   final int usefulLifeYears;
   final DateTime acquisitionDate;
 
-  Asset({this.id, required this.name, required this.value, required this.usefulLifeYears, required this.acquisitionDate});
+  Asset({
+    this.id,
+    required this.name,
+    required this.value,
+    required this.usefulLifeYears,
+    required this.acquisitionDate,
+  });
 
   double get monthlyDepreciation => value / (usefulLifeYears * 12);
 
-  factory Asset.fromMap(Map<String, dynamic> map) { ... }
-  Map<String, dynamic> toMap() { ... }
+  factory Asset.fromMap(Map<String, dynamic> map) {
+    return Asset(
+      id: map['id'] as int?,
+      name: map['name'] as String,
+      value: (map['value'] as num).toDouble(),
+      usefulLifeYears: map['useful_life_years'] as int,
+      acquisitionDate: DateTime.parse(map['acquisition_date'] as String),
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'value': value,
+      'useful_life_years': usefulLifeYears,
+      'acquisition_date': acquisitionDate.toIso8601String(),
+    };
+  }
 }
