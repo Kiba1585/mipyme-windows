ceb8d5692c840d3320d6aa3fb8ba80b25fc4352f
Kiba1585 <122737594+Kiba1585@users.noreply.github.com>
2026-05-15 06:52:58 +0200

    Actualizar main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'screens/activation_screen.dart';
 import 'core/theme/app_theme.dart';
 import 'services/alert_service.dart';
@@ -10,8 +11,34 @@ void main() {
   runApp(const MipymeWindowsApp());
 }
 
-class MipymeWindowsApp extends StatelessWidget {
+class MipymeWindowsApp extends StatefulWidget {
   const MipymeWindowsApp({super.key});
+
+  @override
+  State<MipymeWindowsApp> createState() => _MipymeWindowsAppState();
+}
+
+class _MipymeWindowsAppState extends State<MipymeWindowsApp> {
+  final _storage = const FlutterSecureStorage();
+  ThemeMode _themeMode = ThemeMode.light;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadTheme();
+  }
+
+  Future<void> _loadTheme() async {
+    final darkMode = await _storage.read(key: 'dark_mode');
+    if (darkMode == 'true') {
+      setState(() => _themeMode = ThemeMode.dark);
+    }
+  }
+
+  void changeTheme(bool isDark) {
+    setState(() => _themeMode = isDark ? ThemeMode.dark : ThemeMode.light);
+    _storage.write(key: 'dark_mode', value: isDark.toString());
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,8 +47,8 @@ class MipymeWindowsApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.light,
-      home: const ActivationScreen(),
+      themeMode: _themeMode,
+      home: ActivationScreen(onThemeChanged: changeTheme),
     );
   }
 }
